af6730471942a999ac9fdd062229fb7892711af0
DESKTOP-JGHD1EA\Iair Bluer <iairbluer@gmail.com>
2018-11-03 17:15:47 +0200

    1. initial app commit

---
+++ controllers/home.js
@@ -0,0 +1,37 @@
+/**
+ * GET /
+ * Home page.
+ */
+exports.index = (req, res) => {
+  res.render('home', {
+    title: 'Home'
+  });
+};
+/**
+ * GET /
+ * Enriched Env page.
+ */
+exports.getEnrichedEnv = (req, res) => {
+  res.render('enrichedEnv', {
+    title: 'Enriched Environment'
+  });
+};
+
+/**
+ * GET /
+ * Enriched Env page.
+ */
+exports.getPersonalArea = (req, res) => {
+  res.render('personalArea', {
+    title: 'Personal Area'
+  });
+};
+/**
+ * GET /
+ * Enriched Env page.
+ */
+exports.getContactTeam = (req, res) => {
+  res.render('contactTeam', {
+    title: 'Contact Medical Team'
+  });
+};
